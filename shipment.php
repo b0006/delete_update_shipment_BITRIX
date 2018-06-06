73e004c0a4367998ddbc113faa583577a7659b55
Alexandr Kasikov <qsefthuken@gmail.com>
2018-06-06 11:14:04 +0700

    Update shipment.php

--- shipment.php
+++ shipment.php
@@ -1,23 +1,18 @@
-#!/usr/bin/php
 <?php
-//$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/pokrov.loc";
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-
 CModule::IncludeModule("sale");
 
 $arFilter = Array(
     "MARKED" => "Y",
 );
-
 $db_sales = CSaleOrder::GetList(array("ID" => "ASC"), $arFilter);
+
 while ($ar_sales = $db_sales->Fetch()) {
 
     $order = \Bitrix\Sale\Order::load($ar_sales["ID"]);
     $shipmentCollection = $order->getShipmentCollection();
 
     foreach ($shipmentCollection as $shipment) {
-        $shipment_id = $shipment->getId();
-
         //пропускаем системные
         if ($shipment->isSystem())
             continue;
@@ -25,13 +20,12 @@ while ($ar_sales = $db_sales->Fetch()) {
         //получаем Коллекцию Товаров в Корзине каждой Отгрузки
         $shipmentItemCollection = $shipment->getShipmentItemCollection();
 
-        foreach ($shipmentItemCollection as $item) {
+        foreach ($shipmentItemCollection as $item)
             $item->delete();
-        }
 
         $shipment->setField('ALLOW_DELIVERY', 'Y');
         $shipment->setField('DEDUCTED', 'Y');
-
+        
         $order->save();
     }
 }
